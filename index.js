8d11e33bcc6dabc46288fd32dbff7df8821cc227
Subham Agarwal <subham301477@gmail.com>
2020-12-29 19:24:53 +0530

    add /me APIs

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const server = http.createServer((req, res) => {
         res.write("Hello World from 'POST'!");
         res.end();
     }
+
+    if (req.url.startsWith('/me')) {
+
+        const urlAfterSlashMe = req.url.substr(3);
+
+        if (urlAfterSlashMe.startsWith("?")) {
+            // The requested url was "/me?name=<ANY_STRING>"
+            const namePart = urlAfterSlashMe.substr(6);
+            res.write(namePart);
+            res.end();
+        }
+        else {
+            // The requested url was something like "/me/<ANY_STRING>"
+            const namePart = urlAfterSlashMe.substr(1);
+            res.write(namePart);
+            res.end();
+        }
+    }
 });
 
 server.listen(7050);
